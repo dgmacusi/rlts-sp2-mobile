8f90d841631addb678192197d3a996adf3260cbd
perfeliclemente <dgmacusi@gmail.com>
2018-04-29 23:35:43 +0800

    Working retrieval of Facility timelog

--- app/src/main/java/com/app/rlts/fragment/FacilityFragment.java
+++ app/src/main/java/com/app/rlts/fragment/FacilityFragment.java
@@ -1,5 +1,6 @@
 package com.app.rlts.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,18 +9,34 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.app.rlts.R;
+import com.app.rlts.activity.TimelogActivity;
+import com.app.rlts.entity.Timelog;
+import com.app.rlts.interfaces.AsyncTimelogResponse;
+import com.app.rlts.task.AsyncGetFacilityTimelogTask;
 
-public class FacilityFragment extends Fragment {
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class FacilityFragment extends Fragment implements AsyncTimelogResponse {
 
     View inflateView;
     Button searchButton;
+
+    ArrayList<Timelog> facilityTimelog = new ArrayList<>();
+
+    EditText facilty_date;
+    EditText facility_roomname;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         inflateView = inflater.inflate(R.layout.fragment_facility, container, false);
+
+        facilty_date = (EditText) inflateView.findViewById(R.id.facility_date);
+        facility_roomname = (EditText) inflateView.findViewById(R.id.facility_roomname);
 
         searchButton = (Button) inflateView.findViewById(R.id.facility_search);
 
@@ -27,9 +44,36 @@ public class FacilityFragment extends Fragment {
             @Override
             public void onClick(View v) {
 
+                String date = facilty_date.getText().toString();
+                String room_name = facility_roomname.getText().toString();
+
+                new AsyncGetFacilityTimelogTask(FacilityFragment.this).execute(date, room_name);
+
             }
         });
 
         return inflateView;
+    }
+
+    @Override
+    public void retrieveTimelog(ArrayList<Timelog> tArray) {
+
+        try{
+            facilityTimelog.clear();
+            for(int i = 0; i < tArray.size(); i++){
+                this.facilityTimelog.add(tArray.get(i));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        Intent i = new Intent(getActivity(), TimelogActivity.class);
+
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("timelogArray", (Serializable)facilityTimelog);
+
+        i.putExtras(bundle);
+        i.putExtra("fragment", "facility");
+        startActivity(i);
     }
 }
